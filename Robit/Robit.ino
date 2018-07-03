c19b54e7fb5bf5a67ab9b579ce682dcb14ea36d6
Kevin Huang <13681749+kevinh42@users.noreply.github.com>
2018-07-03 15:23:22 -0700

    Added state switching
    Implemented state switching using an enum.
    
    Moved Tape Following to its own function and added early IR Beacon reading.

--- Robit/Robit.ino
+++ Robit/Robit.ino
@@ -6,57 +6,69 @@
 #define RIGHT_MOTOR 1
 #define LEFT_LF_QRD 4
 #define RIGHT_LF_QRD 5
+#define TEN_KHZ_PIN 6 //analog 1
+#define ONE_KHZ_PIN 7 //analog 0
 
-uint8_t leftSpeed,rightSpeed;
-double error,prevErr,Out;
+uint16_t tenkhzread, onekhzread;
+uint8_t leftSpeed, rightSpeed;
+double error, prevErr, Out;
 double errSum;
 uint32_t prevTime;
 boolean left, right;
-boolean stopped = 0;
+
+enum RobotState{
+  State_TapeFollow,
+  State_IRDetect,
+  State_Testing
+};
+RobotState statecontrol;
 
 class MenuItem
 {
 public:
-  String    Name;
-  uint8_t  Value;
-  uint16_t* EEPROMAddress;
+  String Name;
+  uint8_t Value;
+  uint16_t *EEPROMAddress;
   static uint16_t MenuItemCount;
   MenuItem(String name)
   {
     MenuItemCount++;
-    EEPROMAddress = (uint16_t*)(2 * MenuItemCount);
-    Name      = name;
-    Value         = eeprom_read_word(EEPROMAddress);
+    EEPROMAddress = (uint16_t *)(2 * MenuItemCount);
+    Name = name;
+    Value = eeprom_read_word(EEPROMAddress);
   }
   void Save()
   {
     eeprom_write_word(EEPROMAddress, Value);
   }
 };
- 
+
 uint16_t MenuItem::MenuItemCount = 0;
 /* Add the menu items here */
-MenuItem MotorMax         = MenuItem("MotorMax");
-MenuItem MotorBase        = MenuItem("MotorBase");
+MenuItem MotorMax = MenuItem("MotorMax");
+MenuItem MotorBase = MenuItem("MotorBase");
 MenuItem ProportionalGain = MenuItem("P-gain");
-MenuItem DerivativeGain   = MenuItem("D-gain");
-MenuItem IntegralGain     = MenuItem("I-gain");
-MenuItem menuItems[]      = {MotorMax, MotorBase, ProportionalGain, DerivativeGain, IntegralGain};
+MenuItem DerivativeGain = MenuItem("D-gain");
+MenuItem IntegralGain = MenuItem("I-gain");
+MenuItem menuItems[] = {MotorMax, MotorBase, ProportionalGain, DerivativeGain, IntegralGain};
 
 void setup()
 {
 #include <phys253setup.txt>
-  Serial.begin(9600) ;
-  pinMode(LEFT_MOTOR,OUTPUT);
-  pinMode(RIGHT_MOTOR,OUTPUT);
-  pinMode(LEFT_LF_QRD,INPUT);
-  pinMode(RIGHT_LF_QRD,INPUT);
+  Serial.begin(9600);
+  pinMode(LEFT_MOTOR, OUTPUT);
+  pinMode(RIGHT_MOTOR, OUTPUT);
+  pinMode(LEFT_LF_QRD, INPUT);
+  pinMode(RIGHT_LF_QRD, INPUT);
+  pinMode(TEN_KHZ_PIN, INPUT);
+  pinMode(ONE_KHZ_PIN, INPUT);
+  statecontrol = State_TapeFollow;
 }
 
 void loop()
 {
   LCD.clear();
-  
+
   if (startbutton())
   {
     delay(100);
@@ -65,65 +77,105 @@ void loop()
       Menu();
     }
   }
-  
+
+  switch (statecontrol)
+  {
+  case State_TapeFollow:
+    TapeFollow();
+    break;
+  case State_IRDetect:
+    TapeFollow();
+    IRBeacon();
+    break;
+  case State_Testing:
+    TapeFollow();
+    break;
+  }
+}
+
+void TapeFollow()
+{
   LCD.print("LS:");
   LCD.print(analogRead(LEFT_LF_QRD));
-  LCD.setCursor(8,0);
+  LCD.setCursor(8, 0);
   LCD.print("RS:");
   LCD.print(analogRead(RIGHT_LF_QRD));
- 
-  if (analogRead(RIGHT_LF_QRD) >100){
+
+  if (analogRead(RIGHT_LF_QRD) > 100)
+  {
     right = 1;
-  } else{
+  }
+  else
+  {
     right = 0;
   }
-  if (analogRead(LEFT_LF_QRD)>100){
+  if (analogRead(LEFT_LF_QRD) > 100)
+  {
     left = 1;
-  } else {
+  }
+  else
+  {
     left = 0;
   }
-  if (left && right){
+  if (left && right)
+  {
     error = 0;
-  } else if (left && !right){
+  }
+  else if (left && !right)
+  {
     error = -1;
-  } else if (!left && right){
+  }
+  else if (!left && right)
+  {
     error = 1;
-  } else {
-    if (prevErr>0){
+  }
+  else
+  {
+    if (prevErr > 0)
+    {
       error = 5;
-    } else {
+    }
+    else
+    {
       error = -5;
     }
   }
   update();
   leftSpeed = MotorBase.Value + Out;
   rightSpeed = MotorBase.Value - Out;
-  if (rightSpeed>MotorMax.Value){
+  if (rightSpeed > MotorMax.Value)
+  {
     rightSpeed = MotorMax.Value;
-  } else if (rightSpeed<0){
+  }
+  else if (rightSpeed < 0)
+  {
     rightSpeed = 0;
   }
-  if (leftSpeed>MotorMax.Value){
+  if (leftSpeed > MotorMax.Value)
+  {
     leftSpeed = MotorMax.Value;
-  } else if (leftSpeed<0){
+  }
+  else if (leftSpeed < 0)
+  {
     leftSpeed = 0;
   }
-  LCD.setCursor(0,1);
-  motor.speed(LEFT_MOTOR,leftSpeed);
-  motor.speed(RIGHT_MOTOR,rightSpeed);
+  LCD.setCursor(0, 1);
+  motor.speed(LEFT_MOTOR, leftSpeed);
+  motor.speed(RIGHT_MOTOR, rightSpeed);
   LCD.print("LM:");
   LCD.print(leftSpeed);
-  LCD.setCursor(8,1);
+  LCD.setCursor(8, 1);
   LCD.print("RM:");
   LCD.print(rightSpeed);
 }
 
-void update(){
+void update()
+{
   unsigned long now = millis();
-  double dtime = (double) (now - prevTime);
+  double dtime = (double)(now - prevTime);
 
   errSum += (error * dtime);
-  double dErr = (error - prevErr)/dtime;
+  double dErr = (error - prevErr) / dtime;
 
   Out = ProportionalGain.Value * error + IntegralGain.Value * errSum + DerivativeGain.Value * dErr;
 
@@ -131,23 +183,40 @@ void update(){
   prevTime = now;
 }
 
+void IRBeacon()
+{
+  tenkhzread = analogRead(TEN_KHZ_PIN);
+  onekhzread = analogRead(ONE_KHZ_PIN);
+  LCD.print("1KHZ:");
+  LCD.print(onekhzread);
+  LCD.setCursor(0, 1);
+  LCD.print("10KHZ:");
+  LCD.print(tenkhzread);
+}
+
 void Menu()
 {
-  LCD.clear(); LCD.home();
+  LCD.clear();
+  LCD.home();
   LCD.print("Entering menu");
   delay(500);
 
-  motor.speed(LEFT_MOTOR,0);
-  motor.speed(RIGHT_MOTOR,0);
+  motor.speed(LEFT_MOTOR, 0);
+  motor.speed(RIGHT_MOTOR, 0);
 
   while (true)
   {
     /* Show MenuItem value and knob value */
     int menuIndex = knob(6) * (MenuItem::MenuItemCount) / 1024;
-    LCD.clear(); LCD.home();
-    LCD.print(menuItems[menuIndex].Name); LCD.print(" "); LCD.print(menuItems[menuIndex].Value);
+    LCD.clear();
+    LCD.home();
+    LCD.print(menuItems[menuIndex].Name);
+    LCD.print(" ");
+    LCD.print(menuItems[menuIndex].Value);
     LCD.setCursor(0, 1);
-    LCD.print("Set to "); LCD.print(knob(7)/4); LCD.print("?");
+    LCD.print("Set to ");
+    LCD.print(knob(7) / 4);
+    LCD.print("?");
     delay(100);
 
     /* Press start button to save the new value */
@@ -156,7 +225,7 @@ void Menu()
       delay(100);
       if (startbutton())
       {
-        menuItems[menuIndex].Value = knob(7)/4;
+        menuItems[menuIndex].Value = knob(7) / 4;
         menuItems[menuIndex].Save();
         delay(250);
       }
@@ -168,7 +237,8 @@ void Menu()
       delay(100);
       if (stopbutton())
       {
-        LCD.clear(); LCD.home();
+        LCD.clear();
+        LCD.home();
         LCD.print("Leaving menu");
         delay(500);
         return;
